cb3f743755b420d244150056746adf9bb4b7bd79
oweitman <oweitman@gmx.de>
2026-07-22 17:39:22 +0200

    enable vis2-widget test

--- test/widgets.test.js
+++ test/widgets.test.js
@@ -1,34 +1,33 @@
 const helper = require('@iobroker/vis-2-widgets-testing');
 const adapterName = require('../package.json').name.split('.').pop();
 
-/* describe('vis-2-widgets-jsontemplate', () => {
+describe('vis-2-widgets-jsontemplate', () => {
     before(async function () {
         this.timeout(250000);
         // install js-controller, web and vis-2-beta
         await helper.startIoBroker();
         await helper.startBrowser(process.env.CI === 'true' ? 'new' : false);
-        await helper.createProject(undefined, 50000);
+        await helper.createProject();
 
         // open widgets
-        await helper.palette.openWidgetSet(null, "jsontemplate", 50000);
+        await helper.palette.openWidgetSet(null, adapterName);
         await helper.screenshot(null, '02_widgets_opened');
     });
 
-     it('Check all widgets', async function () {
+    it('Check all widgets', async function () {
         this.timeout(60000);
         const widgets = await helper.palette.getListOfWidgets(null, adapterName);
         for (let w = 0; w < widgets.length; w++) {
-            const wid = await helper.palette.addWidget(null, widgets[w], 5000);
+            const wid = await helper.palette.addWidget(null, widgets[w], true);
             await helper.screenshot(null, `10_${widgets[w]}`);
             await helper.view.deleteWidget(null, wid);
         }
         return Promise.resolve();
-    }); 
+    });
 
     after(async function () {
-        this.timeout(50000);
+        this.timeout(5000);
         await helper.stopBrowser();
         return helper.stopIoBroker();
     });
-}); 
-*/
+});
